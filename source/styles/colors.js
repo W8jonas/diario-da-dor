1f254a340421e47f437a8539e1aba213e90b685e
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-07-19 23:42:36 -0300

    add supportColors

--- source/styles/colors.js
+++ source/styles/colors.js
@@ -3,8 +3,13 @@
 export const colors = {
     
     colorsPrimaryTheme: {
-        primary:  '',
-        secondary:  '',
+        primary:  'rgba(54, 213, 133, 0.5)',
+        secondary:  'rgba(86, 198, 171, 1)',
     },
     
+    supportColors: {
+        primary:  'rgba(0, 188, 210, 1)',
+        secondary:  'rgba(54, 213, 133, 0.5)',
+    },
+
 }
