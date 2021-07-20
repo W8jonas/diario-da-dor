d7526923defb12cecf8e949f9edc7a9bffebaf61
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-07-19 23:37:33 -0300

    criando e estruturando arquivo de rotas

---
+++ source/routes.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { HowYou } from './screens/HowYou'
+
+// Modules
+
+// Assets
+
+// Functions
+
+// Components
+
+export function Routes() {
+    return (
+        <HowYou />
+    )
+}
+
